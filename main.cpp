b3396b3c41a3f0b73732185adfcc02edd94ae459
ElmaddinAbasov <elmaddin.abasov1996@mail.ru>
2025-05-26 15:00:46 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#if 1
 #include "file_handler.h"
 int main()
 {
@@ -6,21 +5,3 @@ int main()
 	fh.modify_file();
 	exit(0);
 }
-#else
-#include <iostream>
-#include <stdio.h>
-#include <ctime>
-int main() {
-    time_t start_time = time(NULL);
-    int duration = 5;
-
-    printf("Timer started for %d seconds...\n", duration);
-
-    while (difftime(time(NULL), start_time) < duration) {
-        // Code to execute during timer
-    }
-
-    printf("Timer is up!\n");
-    return 0;
-}
-#endif
